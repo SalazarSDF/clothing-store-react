fdd23195b4a7cd5096ff91c27bb5bd66837670b7
SalazarSDF <q37076111@gmail.com>
2022-08-06 17:38:45 +0300

    add: redux-thunk to handle async methods

---
+++ src/components/spiner/spiner.component.jsx
@@ -0,0 +1,9 @@
+import { SpinnerOverlay, SpinnerContainer } from "./spiner.styles";
+
+const Spinner = () => (
+  <SpinnerOverlay>
+    <SpinnerContainer></SpinnerContainer>
+  </SpinnerOverlay>
+);
+
+export default Spinner;
