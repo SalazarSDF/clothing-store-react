5d7e271127f07625ef79dd57595ae4e11017c99c
SalazarSDF <q37076111@gmail.com>
2022-08-22 17:23:20 +0300

    add optimization: memo

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useSelector } from "react-redux";
 //import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
@@ -16,9 +17,9 @@ const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
 
-  const goToCheckoutHandler = () => {
+  const goToCheckoutHandler = useCallback(() => {
     navigate("/checkout");
-  };
+  }, [navigate]);
 
   return (
     <CartDropdownContainer>
--- src/components/cart-item/cart-item.component.tsx
+++ src/components/cart-item/cart-item.component.tsx
@@ -1,11 +1,11 @@
-import { FC } from "react";
+import { FC, memo } from "react";
 import { CartItemContainer, ItemDetails } from "./cart-item.styles";
 import { CartItem as TCartItem } from "../../store/cart/cart.types";
 
 type CartItemProps = {
   cartItem: TCartItem;
 };
-const CartItem: FC<CartItemProps> = ({ cartItem }) => {
+const CartItem: FC<CartItemProps> = memo(({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
     <CartItemContainer>
@@ -18,6 +18,6 @@ const CartItem: FC<CartItemProps> = ({ cartItem }) => {
       </ItemDetails>
     </CartItemContainer>
   );
-};
+});
 
 export default CartItem;
